ff5d71d5894b0e5ba9e2fc5e8fd7ee509c74f884
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-05-23 22:42:50 +0530

    Update useInput.js

--- src/components/useInput.js
+++ src/components/useInput.js
@@ -2,7 +2,16 @@ import { useState } from "react";
 
 //complete the input hook
 const useInput = () => {
+    const [value, setValue] = useState("");
+    const onInputChange= (e)=>{
+        setValue(e.target.value)
+    }
+    const bind ={
+        value: value,
+        onChange: onInputChange,
+    }
 
+    return bind;
 };
 
 export default useInput;
